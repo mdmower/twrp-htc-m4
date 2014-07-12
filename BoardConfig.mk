3ae8ed3a3dd26ad8e6634f1f3ae6cefa4900c4b8
Matt Mower <mowerm@gmail.com>
2014-07-12 14:26:48 -0500

    Specify write-offset for bootloader msg in /misc
    HTC stores more than just a bootloader message in /misc. The bootloader
    message is offset to byte 0x800 (2048), verified by choosing "factory
    reset" from hboot, finding where "recovery --wipe_data" was written to
    /misc and then backing up 64bytes, in accordance with struct
    bootloader_message.
    
    Requires the following commits to recovery:
     Do not wipe block misc on bootloader msg update
     I2d2fbdf067282744864a19d404ca7dc12f688a98
    
     Allow custom bootloader msg offset in block misc
     Id13a23dd41bb7d907b96d657b8e21eb839dfeaa9
    
    Change-Id: I1b088f1819ae27cc48ec571047d0f851a68eb425

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,3 +100,4 @@ TW_INCLUDE_JB_CRYPTO := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_HTC_LED := true
 TW_NO_LEGACY_PROPS := true
+BOARD_RECOVERY_BLDRMSG_OFFSET := 2048
