0c8c181581b0e2662d476a77ea4f8ddc162c25b5
Matt Mower <mowerm@gmail.com>
2015-01-30 13:32:22 -0600

    Update configuration for cm-12.0
    Change-Id: Idb00d99dd296f31e451917247306c63247ac4be6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,3 +1,5 @@
+# Copyright (C) 2015 Matthew D. Mower
+# Copyright (C) 2015 The CyanogenMod Project
 # Copyright (C) 2013 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -32,25 +34,22 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
 TARGET_CPU_VARIANT := krait
+TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80600000
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=m4 user_debug=31 androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=m4 androidboot.selinux=permissive user_debug=31
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
 TARGET_KERNEL_CONFIG := m4_defconfig
 TARGET_KERNEL_SOURCE := kernel/htc/msm8960
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
-TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
-TARGET_QCOM_AUDIO_VARIANT := caf
-TARGET_QCOM_DISPLAY_VARIANT := caf
-TARGET_QCOM_MEDIA_VARIANT := caf
-TARGET_USES_QCOM_BSP := true
 
 # Flags
 COMMON_GLOBAL_CFLAGS += -DHTCLOG
+COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
 
 # Graphics
 USE_OPENGL_RENDERER := true
@@ -58,87 +57,44 @@ TARGET_DISPLAY_USE_RETIRE_FENCE := true
 TARGET_USES_C2D_COMPOSITION := true
 TARGET_USES_ION := true
 BOARD_EGL_CFG := device/htc/m4/configs/egl.cfg
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 
 # Power
-TARGET_POWERHAL_VARIANT := cm
-
-# cat /proc/emmc
-#dev:        size     erasesize name
-#mmcblk0p22: 000ffa00 00000200 "misc"
-#mmcblk0p34: 00fffe00 00000200 "recovery"
-#mmcblk0p33: 01000000 00000200 "boot"
-#mmcblk0p35: 85fffc00 00000200 "system"
-#mmcblk0p29: 00140200 00000200 "local"
-#mmcblk0p36: 29fffe00 00000200 "cache"
-#mmcblk0p37: 2d8000000 00000200 "userdata"
-#mmcblk0p25: 01400000 00000200 "devlog"
-#mmcblk0p27: 00040000 00000200 "pdata"
-#mmcblk0p30: 00010000 00000200 "extra"
-#mmcblk0p16: 02d00000 00000200 "radio"
-#mmcblk0p17: 00a00000 00000200 "adsp"
-#mmcblk0p15: 00100000 00000200 "dsps"
-#mmcblk0p18: 00500000 00000200 "wcnss"
-#mmcblk0p19: 007ffa00 00000200 "radio_config"
-#mmcblk0p23: 00400000 00000200 "modem_st1"
-#mmcblk0p24: 00400000 00000200 "modem_st2"
-#mmcblk0p32: 01fffc00 00000200 "reserve"
-#mmcblk0p28: 00004000 00000200 "control"
+TARGET_POWERHAL_VARIANT := qcom
 
 # Filesystem
+TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16776704
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1610611712
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 13220446208
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-# Vold
-BOARD_VOLD_MAX_PARTITIONS := 37
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
-
 # Recovery
 BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
-TARGET_USERIMAGES_USE_EXT4 := true
+
+# Vold
+BOARD_VOLD_MAX_PARTITIONS := 37
+
+# Charge mode
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/htc_lpm/lpm_mode
 
 # SELinux
-BOARD_SEPOLICY_DIRS += device/htc/m4/sepolicy
+-include device/qcom/sepolicy/sepolicy.mk
 
-BOARD_SEPOLICY_UNION += \
-    app.te \
-    bluetooth.te \
-    device.te \
-    domain.te \
-    drmserver.te \
-    file_contexts \
-    file.te \
-    hci_init.te \
-    healthd.te \
-    init_shell.te \
-    init.te \
-    keystore.te \
-    kickstart.te \
-    mediaserver.te \
-    rild.te \
-    surfaceflinger.te \
-    system.te \
-    ueventd.te \
-    wpa_socket.te \
-    wpa.te
+BOARD_SEPOLICY_DIRS += device/htc/m4/sepolicy
 
 # TWRP
 DEVICE_RESOLUTION := 720x1280
-RECOVERY_SDCARD_ON_DATA := true
-BOARD_HAS_NO_REAL_SDCARD := true
-TW_INTERNAL_STORAGE_PATH := "/data/media"
-TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
-TW_EXTERNAL_STORAGE_PATH := "/usb-otg"
-TW_EXTERNAL_STORAGE_MOUNT_POINT := "usb-otg"
-TW_NO_USB_STORAGE := true
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-TW_INCLUDE_JB_CRYPTO := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_INCLUDE_CRYPTO := true
 BOARD_RECOVERY_BLDRMSG_OFFSET := 2048
 RECOVERY_VARIANT := twrp
 TARGET_RECOVERY_DEVICE_MODULES := chargeled
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_NO_USB_STORAGE := true
+TW_EXTERNAL_STORAGE_PATH := "/usb-otg"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "usb-otg"
